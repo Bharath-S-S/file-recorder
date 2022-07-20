42db41f1493dd46b17691a1ad9f26adb39135710
YashwanthSSBrigosha <yashwanth.s@brigosha.com>
2022-07-20 22:09:45 +0530

    calendar changes

--- app/src/main/java/com/example/filerecorder/NewCourseActivity.java
+++ app/src/main/java/com/example/filerecorder/NewCourseActivity.java
@@ -1,19 +1,19 @@
 package com.example.filerecorder;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class NewCourseActivity extends AppCompatActivity {
+import java.util.Calendar;
 
-    // creating a variables for our button and edittext.
-    private EditText courseNameEdt, courseDescEdt, courseDurationEdt;
-    private Button courseBtn;
+public class NewCourseActivity extends AppCompatActivity {
 
     // creating a constant string variable for our
     // course name, description and duration.
@@ -21,6 +21,10 @@ public class NewCourseActivity extends AppCompatActivity {
     public static final String EXTRA_COURSE_NAME = "com.gtappdevelopers.gfgroomdatabase.EXTRA_COURSE_NAME";
     public static final String EXTRA_DESCRIPTION = "com.gtappdevelopers.gfgroomdatabase.EXTRA_COURSE_DESCRIPTION";
     public static final String EXTRA_DURATION = "com.gtappdevelopers.gfgroomdatabase.EXTRA_COURSE_DURATION";
+    // creating a variables for our button and edittext.
+    private EditText fileNmae, dateIn, dateOut;
+    private Button courseBtn;
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +32,9 @@ public class NewCourseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_course);
 
         // initializing our variables for each view.
-        courseNameEdt = findViewById(R.id.idEdtCourseName);
-        courseDescEdt = findViewById(R.id.idEdtCourseDescription);
-        courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
+        fileNmae = findViewById(R.id.fileName);
+        dateIn = findViewById(R.id.dateIn);
+        dateOut = findViewById(R.id.dateOut);
         courseBtn = findViewById(R.id.idBtnSaveCourse);
 
         // below line is to get intent as we
@@ -39,19 +43,55 @@ public class NewCourseActivity extends AppCompatActivity {
         if (intent.hasExtra(EXTRA_ID)) {
             // if we get id for our data then we are
             // setting values to our edit text fields.
-            courseNameEdt.setText(intent.getStringExtra(EXTRA_COURSE_NAME));
-            courseDescEdt.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
-            courseDurationEdt.setText(intent.getStringExtra(EXTRA_DURATION));
+            fileNmae.setText(intent.getStringExtra(EXTRA_COURSE_NAME));
+            dateIn.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            dateOut.setText(intent.getStringExtra(EXTRA_DURATION));
         }
+        dateIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        String selectedDate = day + "/" + (month + 1) + "/" + year;
+                        dateIn.setText(selectedDate);
+                    }
+                };
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(NewCourseActivity.this, onDateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+        dateOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        String selectedDate = day + "/" + (month + 1) + "/" + year;
+                        dateOut.setText(selectedDate);
+                    }
+                };
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(NewCourseActivity.this, onDateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
         // adding on click listener for our save button.
         courseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // getting text value from edittext and validating if
                 // the text fields are empty or not.
-                String courseName = courseNameEdt.getText().toString();
-                String courseDesc = courseDescEdt.getText().toString();
-                String courseDuration = courseDurationEdt.getText().toString();
+                String courseName = fileNmae.getText().toString();
+                String courseDesc = dateIn.getText().toString();
+                String courseDuration = dateOut.getText().toString();
                 if (courseName.isEmpty() || courseDesc.isEmpty() || courseDuration.isEmpty()) {
                     Toast.makeText(NewCourseActivity.this, "Please enter the valid details.", Toast.LENGTH_SHORT).show();
                     return;
